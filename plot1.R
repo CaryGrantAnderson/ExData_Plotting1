cf49a4079100bfe207aaa7b8cf8b0c490547e072
CaryGrantAnderson <Grant@CaryGrantAnderson.com>
2014-08-09 23:12:31 -0400

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -4,37 +4,26 @@
 
 
 ## 1. Read in the unzipped data set.
-
 data_Set_Full <- read.csv("./Data/household_power_consumption.txt", header=T, na.strings="?", sep=';') 
-                    
 
 ## 2. Convert the date string into a true date.
-
-data_Set_Full$Date <- as.Date(data_Set_Full$Date)
-
+data_Set_Full$Date <- as.Date(data_Set_Full$Date, format="%d/%m/%Y")
 
 ## 3. Subset the data to get only the data with dates between 2/1/2007 and 2/2/2007.
-
 data_Set_Feb_1_To_2 <- subset(data_Set_Full, subset=(Date >= "2007-02-01" & Date <= "2007-02-02"))
 
-
 ## 4. Remove the full data set from memory.
-
 rm(data_Set_Full)
 
+## 5. Create the DateTime column for the plot.
+data_Set_Feb_1_To_2$Datetime <- as.POSIXct(paste(as.Date(data_Set_Feb_1_To_2$Date), data_Set_Feb_1_To_2$Time))
 
-## 5. Create Plot 1 - Global Active Power.
-
+## 6. Create Plot 1 - Global Active Power.
 hist(data_Set_Feb_1_To_2$Global_active_power, main="Global Active Power", 
      xlab="Global Active Power (kilowatts)", ylab="Frequency", col="Red")
 
-
-## 6. Save the plot to a PNG file.
-
+## 7. Save the plot to a PNG file.
 dev.copy(png, file="plot1.png", height=480, width=480)
 
-
-## 7. Unload the graphic device.
-
+## 8. Unload the graphic device.
 dev.off()
-
